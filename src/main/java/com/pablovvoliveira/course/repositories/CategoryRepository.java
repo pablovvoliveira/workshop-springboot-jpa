925efe933bb1e8fa875e71b420806e1ce821ffad
Pablo Oliveira <pablovvoliveira@gmail.com>
2023-05-15 10:31:07 -0300

    Category

---
+++ src/main/java/com/pablovvoliveira/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.pablovvoliveira.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pablovvoliveira.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
