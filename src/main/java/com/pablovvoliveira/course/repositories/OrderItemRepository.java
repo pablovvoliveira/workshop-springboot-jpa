5f23e9c3773d521f3a3349d6208a73a406a2dcae
Pablo Oliveira <pablovvoliveira@gmail.com>
2023-05-16 00:44:52 -0300

    OrderItem, many-to-many association with extra attributes pt 2

---
+++ src/main/java/com/pablovvoliveira/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.pablovvoliveira.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pablovvoliveira.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+
+}
